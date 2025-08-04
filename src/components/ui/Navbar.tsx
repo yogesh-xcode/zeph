7c4d474ce524245634ee24cb77cdec29c8916ca2
Yogesh <yogesh.xcode@gmail.com>
2025-08-04 12:01:58 +0530

    feat: add Logirent font and Navbar component; rename button.tsx to Button.tsx

---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./Button";
+import { ArrowRight } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-100">
+          <Link href="/" className="flex z-40 font-logirent  font-bold">
+            Zeph
+          </Link>
+
+          {/* Todo: add mobile navbar */}
+
+          <div className="hidden items-center space-x-4 sm:flex">
+            <>
+              <Link
+                href="/pricing"
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "sm",
+                })}
+              >
+                Pricing
+              </Link>
+              <Link
+                href="/signin"
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "sm",
+                })}
+              >
+                Sign in
+              </Link>
+              <Link
+                className={buttonVariants({
+                  size: "sm",
+                  className: "text-sm",
+                })}
+                href="/signin"
+                target="_blank"
+              >
+                Get started <ArrowRight />
+              </Link>
+            </>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+
+export default Navbar;
